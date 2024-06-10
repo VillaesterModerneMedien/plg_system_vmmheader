b7b77b34e80c426227537357d86c02b7c23c3a78
Whykiki <info@whykiki.de>
2024-06-10 22:08:16 +0200

    Init commit

---
+++ vmmheader.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.log
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+use Joomla\CMS\Factory;
+    use Joomla\Utilities\ArrayHelper;
+
+    defined('_JEXEC') or die;
+
+/**
+ * Joomla! System Logging Plugin.
+ *
+ * @since  1.5
+ */
+class PlgSystemVmmheader extends JPlugin
+{
+
+    protected $app;
+    protected $document;
+
+
+    public function onAfterRender()
+    {
+
+        $app    = Factory::getApplication();
+
+        if($app->isClient('site'))
+        {
+
+            $params = $this->params;
+
+            $headerCode = $params->get('head');
+            $bodyCode = $params->get('body');
+
+            $updatedHtmlContentHead = preg_replace('/(<head[^>]*>)/i', '$1' . $headerCode, $app->getBody(), 1);
+
+
+            $updatedHtmlContentBody = preg_replace('/(<body[^>]*>)/i', '$1' . $bodyCode, $updatedHtmlContentHead);
+
+            $app->setBody($updatedHtmlContentBody);
+        }
+    }
+}
