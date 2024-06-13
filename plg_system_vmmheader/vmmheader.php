464db1ed3ab533a4ec31af3df43cc72c57fc5b52
Whykiki <info@whykiki.de>
2024-06-13 14:54:32 +0200

    V 1.0.2

--- plg_system_vmmheader/vmmheader.php
+++ plg_system_vmmheader/vmmheader.php
@@ -8,40 +8,42 @@
  */
 
 use Joomla\CMS\Factory;
-    use Joomla\Utilities\ArrayHelper;
+use Joomla\CMS\Plugin\CMSPlugin;
 
-    defined('_JEXEC') or die;
+defined('_JEXEC') or die;
 
 /**
  * Joomla! System Logging Plugin.
  *
  * @since  1.5
  */
-class PlgSystemVmmheader extends JPlugin
+class PlgSystemVmmheader extends CMSPlugin
 {
-
-    protected $app;
-    protected $document;
 
 
     public function onAfterRender()
     {
         $app = Factory::getApplication();
 
-        if ($app->isClient('site')) {
-            $params = $this->params;
-
-            $headerCode = $params->get('head');
-            $bodyCode = $params->get('body');
-            $footerCode = $params->get('footer');
-
-            $updatedHtmlContentHead = preg_replace('/(<head[^>]*>)/i', '$1' . $headerCode, $app->getBody(), 1);
-
-            $updatedHtmlContentBody = preg_replace('/(<body[^>]*>)/i', '$1' . $bodyCode, $updatedHtmlContentHead);
-
-            $updatedHtmlContentFooter = preg_replace('/(<\/body>)/i', $footerCode . '$1', $updatedHtmlContentBody);
-
-            $app->setBody($updatedHtmlContentFooter);
+        if($app->isClient('administrator')){
+            return;
         }
+
+        $bodyContent = $app->getBody();
+
+        $params = $this->params;
+
+        $headerCode = $params->get('head');
+        $bodyCode = $params->get('body');
+        $footerCode = $params->get('footer');
+
+        $updatedHtmlContentHead = preg_replace('/(<head[^>]*>)/i', '$1' . $headerCode, $bodyContent, 1);
+
+        $updatedHtmlContentBody = preg_replace('/(<body[^>]*>)/i', '$1' . $bodyCode, $updatedHtmlContentHead);
+
+        $updatedHtmlContentFooter = preg_replace('/(<\/body>)/i', $footerCode . '$1', $updatedHtmlContentBody);
+
+        $app->setBody($updatedHtmlContentFooter);
+
     }
 }
